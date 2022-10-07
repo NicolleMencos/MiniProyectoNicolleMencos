22d9f539506dbc77bdf7bf84041c2e0543232805
Guilherme Santos <guilherme.ssilva@protonmail.com>
2022-10-07 11:13:57 -0300

    Modificación del menú de la aplicación

--- Agenda1.py
+++ Agenda1.py
@@ -102,14 +102,17 @@ def crearReporte():
 def main():
     op = 0
     while op != 7:
-        print("--------------------AGENDA 2022-----------------------")
-        print("1. Crear contacto")
-        print("2. Buscar contacto")
-        print("3. Ver contactos")
-        print("4. Modificar contacto")
-        print("5. Eliminar contacto")
-        print("6. Crear reporte en HTML")
-        print("7. Salir del programa\n\n")
+        print("+--------------------------+")
+        print("|       Agenda 2022        |")
+        print("|--------------------------|")
+        print("| 1. Crear contacto        |")
+        print("| 2. Buscar contacto       |")
+        print("| 3. Ver contactos         |")
+        print("| 4. Modificar contacto    |")
+        print("| 5. Eliminar contacto     |")
+        print("| 6. Crear reporte en HTML |")
+        print("| 7. Salir del programa    |")
+        print("+--------------------------+\n")
         op = int(input("Ingrese el número de opción: "))
         if op == 1:
             numero = int(input("Ingrese el número de teléfono: "))
